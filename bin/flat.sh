9627eedb0c611e4c336eb87dcab3eccd003e6410
echo <hujw77@gmail.com>
2024-01-03 11:21:24 +0800

    OracleV2

--- bin/flat.sh
+++ bin/flat.sh
@@ -2,6 +2,9 @@
 
 set -eo pipefail
 
+rm -rf flat/
+mkdir -p flat/
+
 forge flatten src/ORMP.sol -o ./flat/ORMP.f.sol
-forge flatten src/eco/Oracle.sol -o ./flat/Oracle.f.sol
+forge flatten src/eco/OracleV2.sol -o ./flat/OracleV2.f.sol
 forge flatten src/eco/Relayer.sol -o ./flat/Relayer.f.sol
